0953755d36474eb0a7206a06d2bf73bf9efda668
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-03-09 10:08:52 +0300

    showing admin or usper admin role

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,12 +8,7 @@ function NavBar() {
   const Auth = useSelector((state) => state.Auth.Auth);
   const dispatch = useDispatch();
   return (
-    <div
-      className="
- 
-    
-    bg-gray-800 text-white flex justify-between items-center p-4"
-    >
+    <div className="bg-gray-800 text-white flex justify-between items-center px-4 py-2">
       <div>
         <h1 className="text-2xl font-bold">Enlightors</h1>
       </div>
@@ -22,7 +17,16 @@ function NavBar() {
         <Menu as="div" className="relative inline-block text-left">
           <div>
             <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-              {Auth.name}
+              <div className="flex-col items-start flex">
+                {Auth.name}
+                <p className=" text-sm font-medium text-gray-300">
+                  {Auth.role === "admin" ? (
+                    <span className="text-green-500">Admin</span>
+                  ) : (
+                    <span className="text-blue-500">Super Admin</span>
+                  )}
+                </p>
+              </div>
               <ChevronDownIcon
                 className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
                 aria-hidden="true"
@@ -38,7 +42,7 @@ function NavBar() {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 mt-2 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="px-1 py-1">
                 <Menu.Item>
                   {({ active }) => (
@@ -61,7 +65,7 @@ function NavBar() {
                           aria-hidden="true"
                         />
                       )}
-                      Profile
+                      Sign Out
                     </button>
                   )}
                 </Menu.Item>
